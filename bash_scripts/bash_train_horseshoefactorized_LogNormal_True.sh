68c2a6668f947258de6e7f95d5f91f08dc4d3ce7
Akanni96 <18564167+Akanni96@users.noreply.github.com>
2020-02-17 20:05:04 +0000

    Syncing Servers

--- bash_scripts/bash_train_horseshoefactorized_LogNormal_True.sh
+++ bash_scripts/bash_train_horseshoefactorized_LogNormal_True.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ../../mypython36/bin/activate
 until CUDA_VISIBLE_DEVICES=3 python3 train.py -mts "{'stochastic':True,'stochastic_f_pass':10,'distr_type':'LogNormal','discrete_continuous':True,'precip_threshold':0.5,'var_model_type':'horseshoefactorized' }" -mn "DeepSD" -gidx "[0]" -dd "/media/Data3/akanni/Vandal/Data"; do
     echo " task.py crashed with exit code $?.  Restarting.." >&2
     sleep 10
