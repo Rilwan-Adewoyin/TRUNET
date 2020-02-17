5cbf2c5ca4604b4544d8dc4bd8f0dd0156ba64f7
Akanni96 <18564167+Akanni96@users.noreply.github.com>
2020-02-17 19:39:50 +0000

    Syncing Servers

--- train.py
+++ train.py
@@ -323,8 +323,10 @@ def train_loop(train_params, model_params):
                 elif(model_params['model_type_settings']['var_model_type'] in ['flipout']):
                     gradients_clipped_global_norm, _ = tf.clip_by_global_norm(gradients, model_params['gradients_clip_norm']*2.5 ) 
                 else:
-                    gradients_clipped_global_norm, _ = tf.clip_by_global_norm(gradients, model_params['gradients_clip_norm'] )
-            
+                    gradients_clipped_global_norm, _ = tf.clip_by_global_norm(gradients, model_params['gradients_clip_norm']*2.5 )
+                    if tf.math.reduce_any( tf.math.is_nan( gradients_clipped_global_norm[0] ) ):
+                        gradients_clipped_global_norm = gradients
+                        
             optimizer.apply_gradients( zip( gradients_clipped_global_norm, model.trainable_variables ) )
             
             #region Tensorboard Update
