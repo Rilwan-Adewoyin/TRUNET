4de8f61afbb71db14f67d17ee476708e444a4bc0
Akanni96 <18564167+Akanni96@users.noreply.github.com>
2020-02-24 12:58:23 +0000

    syncing server

--- train.py
+++ train.py
@@ -145,10 +145,12 @@ def train_loop(train_params, model_params):
         
     else:
         batches_to_skip = int(df_batch_record.iloc[0])
-        if batches_to_skip == train_params['train_set_size_batches'] :
+        if batches_to_skip >= train_params['train_set_size_batches'] :
             starting_epoch = starting_epoch + 1
             batches_to_skip = 0
+    
     print("batches to skip", batches_to_skip)
+
     #batches_to_skip_on_error = 2
     # endregion
 
