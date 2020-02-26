5c9fcdd6c615f7977dfdaea86d39ec2583771188
Akanni96 <18564167+Akanni96@users.noreply.github.com>
2020-02-26 19:28:53 +0000

    Implemented Training for SimpleConvLSTM

--- train.py
+++ train.py
@@ -149,9 +149,9 @@ def train_loop(train_params, model_params):
         
     else:
         batches_to_skip = int(df_batch_record.iloc[0])
-        if batches_to_skip >= train_params['train_set_size_batches'] :
+        if batches_to_skip >= train_set_size_batches :
             starting_epoch = starting_epoch + 1
-            batches_to_skip = train_params['train_set_size_batches']
+            batches_to_skip = train_set_size_batches
     
     print("batches to skip", batches_to_skip)
 
