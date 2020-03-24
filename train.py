08b60650fc6b593e0e264f2ba5e2fbd45ad35564
akanni.ade <akanni.ade>
2020-03-24 03:06:59 +0000

    Sync Server

--- train.py
+++ train.py
@@ -114,18 +114,18 @@ def train_loop(train_params, model_params):
     if model_params['model_type_settings']['discrete_continuous'] == True:
         #Trying 2 optimizers for discrete_continuious LSTM
         if model_params['model_type_settings']['model_version'] in ["54","55","56"]:
-            # optimizer_rain = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-2 , "warmup_proportion":0.6,"min_lr":1e-3, "beta_1":0.05, "beta_2":0.85, "decay":0.95,
-            #                                     "amsgrad":True} , total_steps=total_steps ) 
-            # optimizer_nonrain = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-3 , "warmup_proportion":0.6,"min_lr":1e-4, "beta_1":0.05, "beta_2":0.99, "decay":0.95,
-            #                                                 "amsgrad":True} , total_steps=total_steps ) #copy.deepcopy( optimizer )
-            # optimizer_dc = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-2 , "warmup_proportion":0.6,"min_lr":1e-3, "beta_1":0.05, "beta_2":0.99, "decay":0.95,
-            #                                                 "amsgrad":True}, total_steps=total_steps )  #copy.deepcopy( optimizer )
+            optimizer_rain = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-2 , "warmup_proportion":0.6,"min_lr":1e-3, "beta_1":0.05, "beta_2":0.85, "decay":0.95,
+                                                "amsgrad":True} , total_steps=total_steps ) 
+            optimizer_nonrain = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-4 , "warmup_proportion":0.6,"min_lr":1e-4, "beta_1":0.05, "beta_2":0.99, "decay":0.95,
+                                                            "amsgrad":True} , total_steps=total_steps ) #copy.deepcopy( optimizer )
+            optimizer_dc = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-3 , "warmup_proportion":0.6,"min_lr":1e-3, "beta_1":0.05, "beta_2":0.99, "decay":0.95,
+                                                            "amsgrad":True}, total_steps=total_steps )  #copy.deepcopy( optimizer )
 
-            optimizer_rain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-2, "beta_1":0.01, "beta_2":0.85, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1  } ) #Every 30 epochs
+            # optimizer_rain = tf.keras.optimizers.Nadam(     **{"learning_rate":1e-2, "beta_1":0.01, "beta_2":0.85, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1  } ) #Every 30 epochs
 
-            optimizer_nonrain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-5,"beta_1":0.01, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 }  ) 
+            # optimizer_nonrain = tf.keras.optimizers.Nadam(  **{"learning_rate":1e-4,"beta_1":0.01, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 }  ) 
 
-            optimizer_dc = tf.keras.optimizers.Nadam( **{"learning_rate":1e-3,"beta_1":0.01, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 } ) 
+            # optimizer_dc = tf.keras.optimizers.Nadam(       **{"learning_rate":1e-3,"beta_1":0.01, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 } ) 
             
             
             optimizers = [optimizer_rain, optimizer_nonrain, optimizer_dc]
