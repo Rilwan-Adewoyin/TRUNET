21c9c0ff7f20b255921df0f1967906ff8ece8754
akanni.ade <akanni.ade>
2020-03-24 01:57:47 +0000

    Sync Server

--- train.py
+++ train.py
@@ -121,11 +121,11 @@ def train_loop(train_params, model_params):
             # optimizer_dc = tfa.optimizers.RectifiedAdam( **{"learning_rate":1e-2 , "warmup_proportion":0.6,"min_lr":1e-3, "beta_1":0.05, "beta_2":0.99, "decay":0.95,
             #                                                 "amsgrad":True}, total_steps=total_steps )  #copy.deepcopy( optimizer )
 
-            optimizer_rain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-3, "beta_1":0.35, "beta_2":0.85, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1  } ) #Every 30 epochs
+            optimizer_rain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-2, "beta_1":0.9, "beta_2":0.88, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1  } ) #Every 30 epochs
 
-            optimizer_nonrain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-6,"beta_1":0.35, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 }  ) 
+            optimizer_nonrain = tf.keras.optimizers.Nadam( **{"learning_rate":1e-5,"beta_1":0.9, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 }  ) 
 
-            optimizer_dc = tf.keras.optimizers.Nadam( **{"learning_rate":1e-4,"beta_1":0.35, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 } ) 
+            optimizer_dc = tf.keras.optimizers.Nadam( **{"learning_rate":1e-3,"beta_1":0.9, "beta_2":0.99, "epsilon":1e-3, "schedule_decay": (30*train_set_size_batches/3)**-1 } ) 
             
             
             optimizers = [optimizer_rain, optimizer_nonrain, optimizer_dc]
