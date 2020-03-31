502547476f6181ead3124cd78dfd844419ceaf61
akanni.ade <akanni.ade>
2020-03-31 14:48:14 +0100

    Trivial: Amend grad clipping

--- train.py
+++ train.py
@@ -720,7 +720,7 @@ def train_loop(train_params, model_params):
                           _optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
                     else:
-                        gradients, _ = tf.clip_by_global_norm( gradients, 10.0 )
+                        gradients, _ = tf.clip_by_global_norm( gradients, 5.0 )
                         _optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
                 gc.collect()
