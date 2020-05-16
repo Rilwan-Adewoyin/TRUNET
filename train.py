a2e52b533487762277d3937665e0f65dadc154da
akanni.ade <akanni.ade>
2020-05-16 21:50:34 +0100

    Trivial update

--- train.py
+++ train.py
@@ -203,6 +203,7 @@ def train_loop(train_params, model_params):
     if ckpt_manager_batch.latest_checkpoint: #restoring last checkpoint if it exists
 
         ckpt_batch.restore(ckpt_manager_batch.latest_checkpoint).assert_consumed()
+        ckpt_batch.restore(ckpt_manager_batch.latest_checkpoint).assert_existing_objects_matched()
         #ckpt_batch.restore(ckpt_manager_epoch.latest_checkpoint).assert_consumed()
 
     else:
