6465593a48aab7fa5882accb2981eaba8117bafc
akanni.ade <akanni.ade>
2020-05-17 03:31:40 +0100

    Trivial update

--- layers.py
+++ layers.py
@@ -821,7 +821,7 @@ class THST_CGRU_Attention_Layer(tf.keras.layers.Layer):
 		compat_dict = {'di':train_params['downscaled_input'] , 'ctsm':train_params['ctsm']}
 		self.convGRU_attn 				= Bidirectional( layer=layers_ConvGRU2D.ConvGRU2D_attn( **CGRU_params,
 															attn_params=attn_params , attn_downscaling_params=attn_downscaling_params ,
-															attn_factor_reduc=seq_len_factor_reduction ,trainable=self.trainable, compact_dict=compat_dict ),
+															attn_factor_reduc=seq_len_factor_reduction ,trainable=self.trainable, compat_dict=compat_dict ),
 
 															backward_layer=layers_ConvGRU2D.ConvGRU2D_attn( go_backwards=True, **copy.deepcopy(CGRU_params),
 																attn_params=attn_params , attn_downscaling_params=attn_downscaling_params ,
