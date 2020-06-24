1cfec02c7d55260ff3bb79e4ff1afec2245dbc1a
akanni.ade <akanni.ade>
2020-06-24 17:33:19 +0100

    fix1

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ class TrainTruNet():
         try:
             self.df_training_info = pd.read_csv( "checkpoints/{}/checkpoint_scores.csv".format(utility.model_name_mkr(m_params,t_params=self.t_params)), header=0, index_col=False) 
             self.start_epoch =  int(max(self.df_training_info['Epoch'], default=0)) 
-            #last_batch = int( self.df_training_info.loc[self.df_training_info['Epoch']==self.start_epoch,'Last_Trained_Batch'].iloc[0] )
+            last_batch = int( self.df_training_info.loc[self.df_training_info['Epoch']==self.start_epoch,'Last_Trained_Batch'].iloc[0] )
             if(last_batch in [-1, self.t_params['train_batches']] ):
                 self.start_epoch = self.start_epoch + 1
                 #self.batches_to_skip = 0
