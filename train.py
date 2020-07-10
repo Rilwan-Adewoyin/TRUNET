63ec78eabe447aac3d18ee0c7412e026c31ecd72
akanni.ade <akanni.ade>
2020-07-10 13:44:20 +0100

    updating optimization params

--- train.py
+++ train.py
@@ -424,14 +424,14 @@ class TrainTruNet():
 
                 if self.m_params['model_type_settings']['distr_type'] == 'Normal': 
                     # CC Normal
-                    loss_to_optimize += 1.2*cl.mse( target_cond_rain, preds_cond_rain, all_count )
+                    loss_to_optimize += 1.1*cl.mse( target_cond_rain, preds_cond_rain, all_count )
                 
                 elif self.m_params['model_type_settings']['distr_type'] == 'LogNormal':    
                     # CC LogNormal                                                             
-                    loss_to_optimize += 1.5*cl.log_mse( target_cond_rain, preds_cond_rain, all_count)                                                         
+                    loss_to_optimize += cl.log_mse( target_cond_rain, preds_cond_rain, all_count)                                                         
                 
-                if r_batch_size == True:
-                    loss_to_optimize += 0.8 * tf.reduce_mean( 
+                if True:
+                    loss_to_optimize += 0.90*tf.reduce_mean( 
                                     tf.keras.backend.binary_crossentropy(labels_true, labels_pred, from_logits=False) ) 
                     loss_for_record = loss_to_optimize
                 
