814c9a0eca489797768aaa3d0a322aaefd4585a1
akanni.ade <akanni.ade>
2020-07-15 16:01:30 +0100

    temp

--- hparameters.py
+++ hparameters.py
@@ -121,8 +121,8 @@ class model_TRUNET_hparameters(MParams):
 
         # ConvGRU params
         if model_type_settings.get('large_model',False) == False:
-            filters = 80 # no. of filters in all conv operations in ConvGRU units
-            #filters = 72 # no. of filters in all conv operations in ConvGRU units
+            #filters = 80 # no. of filters in all conv operations in ConvGRU units
+            filters = 64 # no. of filters in all conv operations in ConvGRU units
 
         else:
             filters = 120
